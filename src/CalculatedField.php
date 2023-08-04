f1def3b9300eda9ecf23e715706d0343fb4fb0cf
qt <quentin@qskousen.info>
2023-08-04 07:22:20 -0500

    Initial commit

---
+++ src/CalculatedField.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Qskousen\CalculatedField;
+
+use Laravel\Nova\Fields\Field;
+
+class CalculatedField extends Field
+{
+    /**
+     * The field's component.
+     *
+     * @var string
+     */
+    public $component = 'calculated-field';
+}
